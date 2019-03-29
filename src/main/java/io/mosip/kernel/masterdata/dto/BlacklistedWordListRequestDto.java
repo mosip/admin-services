2b675e1832549e59ae1a9dc0af81b3225775e33a
NagalakshmiNithyanandan <Nagalakshmi.Nithyanandan@mindtree.com>
2019-03-29 16:51:30 +0530

    merge to 0.10.0 https://mosipid.atlassian.net/browse/merge to
    Former-commit-id: 0ce6cdd3e08355808159da270c04f53d4259d2ed
    Former-commit-id: 5cd7e8f76131600b89bbce8d261c921be7c62fcd
    Former-commit-id: 0fcd03b0a45c708db379c843c1bfc2cb9a2f700a

--- src/main/java/io/mosip/kernel/masterdata/dto/BlacklistedWordListRequestDto.java
+++ src/main/java/io/mosip/kernel/masterdata/dto/BlacklistedWordListRequestDto.java
@@ -8,12 +8,14 @@ import lombok.NoArgsConstructor;
 
 /**
  * 
- * @author Uday Kumar
- * @version 1.0.0
+ * @author Neha Sinha
+ *
  */
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-public class RegCenterMachineUserReqDto<T> {
-	private List<T> request;
+@NoArgsConstructor
+public class BlacklistedWordListRequestDto {
+
+	private List<String> blacklistedwords;
+	
 }
