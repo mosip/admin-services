603e3be55b9426b6e8758e59f3e31ff06a8d80df
Manoj SP <Manoj.SP@mindtree.com>
2021-02-25 11:40:58 +0530

    [MOSIP-12030] Updated logger

--- admin/hotlist-service/src/main/java/io/mosip/hotlist/logger/HotlistLogger.java
+++ admin/hotlist-service/src/main/java/io/mosip/hotlist/logger/HotlistLogger.java
@@ -1,7 +1,6 @@
 package io.mosip.hotlist.logger;
 
 import io.mosip.kernel.core.logger.spi.Logger;
-import io.mosip.kernel.logger.logback.appender.RollingFileAppender;
 import io.mosip.kernel.logger.logback.factory.Logfactory;
 
 /**
@@ -12,23 +11,8 @@ import io.mosip.kernel.logger.logback.factory.Logfactory;
  */
 public final class HotlistLogger {
 
-	private static RollingFileAppender mosipRollingFileAppender;
-
-	static {
-		mosipRollingFileAppender = new RollingFileAppender();
-		mosipRollingFileAppender.setAppend(true);
-		mosipRollingFileAppender.setAppenderName("fileappender");
-		mosipRollingFileAppender.setFileName("logs/hotlist.log");
-		mosipRollingFileAppender.setFileNamePattern("logs/hotlist-%d{yyyy-MM-dd}-%i.log");
-		mosipRollingFileAppender.setImmediateFlush(true);
-		mosipRollingFileAppender.setMaxFileSize("1mb");
-		mosipRollingFileAppender.setMaxHistory(3);
-		mosipRollingFileAppender.setPrudent(false);
-		mosipRollingFileAppender.setTotalCap("10mb");
-	}
-
 	/**
-	 * Instantiates a new id repo logger.
+	 * Instantiates a new ida logger.
 	 */
 	private HotlistLogger() {
 	}
@@ -36,10 +20,11 @@ public final class HotlistLogger {
 	/**
 	 * Method to get the rolling file logger for the class provided.
 	 *
-	 * @param clazz the clazz
+	 * @param clazz
+	 *            the clazz
 	 * @return the logger
 	 */
 	public static Logger getLogger(Class<?> clazz) {
-		return Logfactory.getDefaultRollingFileLogger(mosipRollingFileAppender, clazz);
+		return Logfactory.getSlf4jLogger(clazz);
 	}
 }
