fba7074b9f1bb89b470d3c378e5c7ed437666c6e
kameshsr <kameshsr1338@gmail.com>
2021-06-30 15:34:20 +0530

    MOSIP-15446: openapi.json file generated for admin-services

--- admin/admin-service/src/main/java/io/mosip/admin/config/SwaggerConfig.java
+++ admin/admin-service/src/main/java/io/mosip/admin/config/SwaggerConfig.java
@@ -89,7 +89,7 @@ public class SwaggerConfig {
 		}
 
 		Docket docket = new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
-				.groupName(TITLE).select().apis(RequestHandlerSelectors.any())
+				.select().apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.regex("(?!/(error).*).*")).build();
 		if (swaggerBaseUrlSet) {
 			docket.protocols(protocols()).host(hostWithPort);
