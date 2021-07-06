a7cffe1ad8afcea75c8eacd5de5f070397a94414
Manoj SP <43261486+manojsp12@users.noreply.github.com>
2021-07-06 15:36:39 +0530

    Mosip 12030 - Updated hotlist websub and added event id (#280)
    * [MOSIP-12030] ignoring test cases temporarily
    
    * [MOSIP-12030] updated dockerfile
    
    * [MOSIP-12030] updated docker exposed port
    
    * [MOSP-12030] updated dockerFile
    
    * [MOSIP-12030] updated DockerFile
    
    * [MOSIP-11791] moved common constants to kernel-core
    
    * [MOSIP-14524] updated expiry time to be null instead with year 9999
    
    * [MOSIP-11791] moved common constants to kernel-core
    
    * [MOSIP-14524] updated expiry time to be null instead with year 9999
    
    * [MOSIP-14524] updated event handler
    
    * [MOSIP-14524] updated expiry time to be null instead with year 9999
    
    * [MOSIP-12030] updated dockerFile
    
    * [MOSIP-12030] updated docker exposed port
    
    * [MOSP-12030] updated dockerFile
    
    * [MOSIP-12030] updated dockerFile
    
    * [MOSIP-14524] updated event handler
    
    * [MOSIP-14509] updated hotlist cleanup job properties
    
    * [MOSIP-12030] updated hotlist event publishing
    
    * [MOSIP-12030] reverted cleanup job changes
    
    * [MOSIP-12030] updated publishing timestamp
    
    * [MOSIP-12030] updated publishing timestamp

--- admin/hotlist-service/src/main/java/io/mosip/hotlist/event/HotlistEventHandler.java
+++ admin/hotlist-service/src/main/java/io/mosip/hotlist/event/HotlistEventHandler.java
@@ -3,6 +3,7 @@ package io.mosip.hotlist.event;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -66,8 +67,11 @@ public class HotlistEventHandler {
 			EventModel payload = new EventModel();
 			payload.setPublisher(appId);
 			payload.setTopic(topic);
-			payload.setPublishedOn(DateUtils.getCurrentDateTimeString());
+			String publishedOn = DateUtils.formatToISOString(DateUtils.getUTCCurrentDateTime());
+			payload.setPublishedOn(publishedOn);
 			Event event = new Event();
+			event.setId(UUID.randomUUID().toString());
+			event.setTimestamp(publishedOn);
 			Map<String, Object> data = new HashMap<>();
 			data.put("id", id);
 			data.put("idType", idType);
