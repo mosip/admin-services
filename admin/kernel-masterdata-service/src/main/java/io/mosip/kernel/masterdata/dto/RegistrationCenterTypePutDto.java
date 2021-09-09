0986b31b4ff4e3d540932031d3610ccb25aaa8c8
Govindaraj-035 <govindarajv@nsdl.co.in>
2021-09-09 12:18:32 +0530

    modified mosip-15150

--- admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/RegistrationCenterTypePutDto.java
+++ admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/RegistrationCenterTypePutDto.java
@@ -3,6 +3,7 @@ package io.mosip.kernel.masterdata.dto;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import io.mosip.kernel.masterdata.validator.CenterTypeCode;
 import io.mosip.kernel.masterdata.validator.StringFormatter;
 import io.mosip.kernel.masterdata.validator.ValidLangCode;
 import lombok.AllArgsConstructor;
@@ -18,7 +19,7 @@ public class RegistrationCenterTypePutDto {
 	 * code of the registration center type.
 	 */
 	@NotNull
-	@StringFormatter(min = 1, max = 36)
+	@CenterTypeCode(min =2, max = 36)
 	private String code;
 	/**
 	 * language code of the registration center type.
