12aff7bc928009402398f27daf9f2c91111b7e39
dhanendra06 <dhanendra.tech@gmail.com>
2021-09-15 15:08:20 +0530

    fixed bug for masterdata api's

--- admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/utils/MasterdataCreationUtil.java
+++ admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/utils/MasterdataCreationUtil.java
@@ -4,6 +4,7 @@
 package io.mosip.kernel.masterdata.utils;
 
 import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -23,10 +24,13 @@ import javax.persistence.criteria.Root;
 
 import org.hibernate.HibernateException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import io.mosip.kernel.core.dataaccess.exception.DataAccessLayerException;
+import io.mosip.kernel.core.util.EmptyCheckUtils;
 import io.mosip.kernel.dataaccess.hibernate.constant.HibernateErrorCode;
 import io.mosip.kernel.masterdata.constant.RegistrationCenterErrorCode;
 import io.mosip.kernel.masterdata.exception.MasterDataServiceException;
@@ -48,6 +52,12 @@ public class MasterdataCreationUtil {
 	private static final String ISACTIVE_COLUMN_NAME = "isActive";
 
 	private static final String NAME_COLUMN_NAME = "name";
+	
+	private static final String UPD_BY_COLUMN_NAME = "updatedBy";
+
+	private static final String UPDA_DTIMES_COLUMN_NAME = "updatedDateTime";
+
+	private static String contextUser = "superadmin";
 
 
 	@Value("#{'${mosip.mandatory-languages:}'.concat('${mosip.optional-languages:}')}")
@@ -259,6 +269,10 @@ public class MasterdataCreationUtil {
 	}
 
 	public <E> int updateMasterDataStatus(Class<E> entity, String primaryKeyValue, boolean isActive, String column) {
+		Authentication authN = SecurityContextHolder.getContext().getAuthentication();
+		if (!EmptyCheckUtils.isNullEmpty(authN)) {
+			contextUser = authN.getName();
+		}
 		List<Predicate> predicates = new ArrayList<Predicate>();
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 		CriteriaUpdate<E> update = criteriaBuilder.createCriteriaUpdate(entity);
@@ -267,6 +281,8 @@ public class MasterdataCreationUtil {
 		predicates.add(codePredicate);
 		update.where(predicates.toArray(new Predicate[] {}));
 		update.set(root.get(ISACTIVE_COLUMN_NAME), isActive);
+		update.set(root.get(UPD_BY_COLUMN_NAME), contextUser);
+		update.set(root.get(UPDA_DTIMES_COLUMN_NAME), LocalDateTime.now());
 		Query executableQuery = entityManager.createQuery(update);
 		return executableQuery.executeUpdate();
 	}
