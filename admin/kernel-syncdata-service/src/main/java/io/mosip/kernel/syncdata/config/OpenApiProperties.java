6c668c4da5f85ab166a1e056cd90043c282ffa7b
Govindaraj-035 <govindarajv@nsdl.co.in>
2021-10-25 16:00:59 +0530

    open-api implemented

---
+++ admin/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/config/OpenApiProperties.java
@@ -0,0 +1,72 @@
+package io.mosip.kernel.syncdata.config;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+/**
+ * @author GOVINDARAJ VELU
+ *
+ */
+@Configuration
+@ConfigurationProperties(prefix = "openapi")
+@Data
+public class OpenApiProperties {
+	private InfoProperty info;
+	private Service service;
+	private Group group;
+}
+
+/**
+ * @author GOVINDARAJ VELU
+ *
+ */
+@Data
+class InfoProperty {
+	private String title;
+	private String description;
+	private String version;
+	private LicenseProperty license;
+}
+
+/**
+ * @author GOVINDARAJ VELU
+ *
+ */
+@Data
+class LicenseProperty {
+	private String name;
+	private String url;
+}
+
+/**
+ * @author GOVINDARAJ VELU
+ *
+ */
+@Data
+class Service {
+	private List<Server> servers;
+}
+
+/**
+ * @author GOVINDARAJ VELU
+ *
+ */
+@Data
+class Server {
+	private String description;
+	private String url;
+}
+
+/**
+ * @author GOVINDARAJ VELU
+ *
+ */
+@Data
+class Group {
+	private String name;
+	private List<String> paths;
+}
