da904d47835475fcda63453b7cb48e06a8693179
Ramadevi Idupulapati <M1063027@mindtree.com>
2021-12-24 21:01:57 +0530

    MOSIP-18688

--- admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/ApplicantValidDocumentDto.java
+++ admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/dto/ApplicantValidDocumentDto.java
@@ -1,14 +1,27 @@
-package io.mosip.kernel.masterdata.dto;
+package io.mosip.kernel.syncdata.dto;
 
-import java.util.Collection;
-
-import io.mosip.kernel.masterdata.dto.getresponse.DocumentCategoryAndTypeResponseDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+/**
+ * 
+ * @author Srinivasan
+ *
+ */
 @Data
-public class ApplicantValidDocumentDto {
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApplicantValidDocumentDto extends BaseDto {
+
 	private String appTypeCode;
+
+	private String docTypeCode;
+
+	private String docCatCode;
+
 	private String langCode;
-	private Boolean isActive;
-	private Collection<DocumentCategoryAndTypeResponseDto> documentCategories;
+
 }
