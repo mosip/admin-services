1522d30a4c86b86317fa3b3c89d06f5a57d3893f
Manoj SP <Manoj.SP@mindtree.com>
2022-01-11 14:30:30 +0530

    [MOSIP-17828] removed unused imports

--- admin/hotlist-service/src/main/java/io/mosip/hotlist/dto/AuthorizedRolesDto.java
+++ admin/hotlist-service/src/main/java/io/mosip/hotlist/dto/AuthorizedRolesDto.java
@@ -6,21 +6,18 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
 
 @Component("authorizedRoles")
 @ConfigurationProperties(prefix = "mosip.role.admin.hotlist")
 @Data
 public class AuthorizedRolesDto {
 
-  //Hotlist controller
-	
+	// Hotlist controller
+
 	private List<String> postHotlistBlock;
-	
-    private List<String> getHotlistStatus;	
-	
-	private List<String> postHotlistUnblock; 
-	 
+
+	private List<String> getHotlistStatus;
+
+	private List<String> postHotlistUnblock;
+
 }
