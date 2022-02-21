a1c3a0a4d875888eb9e27c2beb37c799482cbf19
Shilpa M <m1053288@mindtree.com>
2022-02-21 20:17:41 +0530

    resolved test case issue

--- admin/kernel-masterdata-service/src/test/java/io/mosip/kernel/masterdata/test/controller/IntegratedRepositoryTest.java
+++ admin/kernel-masterdata-service/src/test/java/io/mosip/kernel/masterdata/test/controller/IntegratedRepositoryTest.java
@@ -2351,7 +2351,7 @@ public class IntegratedRepositoryTest {
 	@Test
 	@WithUserDetails("global-admin")
 	public void tst022getDynamicFieldByNameTest1() throws Exception {
-		when( dynamicFieldRepository.findAllDynamicFieldByNameAndLangCode(Mockito.anyString(), Mockito.anyString())).thenThrow(new DataAccessException("...") {});
+		when( dynamicFieldRepository.findAllDynamicFieldByNameLangCodeAndisDeleted(Mockito.anyString(), Mockito.anyString())).thenThrow(new DataAccessException("...") {});
 		MasterDataTest.checkResponse(
 				mockMvc.perform(MockMvcRequestBuilders.get("/dynamicfields/blod/eng")).andReturn(),
 				"KER-SCH-001");
