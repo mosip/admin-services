62708580f80c61b814e3b74c0261416d9937792e
dhanendra06 <dhanendra.tech@gmail.com>
2022-06-08 12:51:04 +0530

    MOSIP-22027,MOSIP-22143,MOSIP-22145

--- db_release_scripts/mosip_master/sql/1.2_master-scripts_release.sql
+++ db_release_scripts/mosip_master/sql/1.2_master-scripts_release.sql
@@ -245,6 +245,9 @@ ALTER TABLE master.batch_job_execution_params ALTER COLUMN string_val TYPE varch
 ALTER TABLE master.blocklisted_words DROP CONSTRAINT IF EXISTS pk_blwrd_code;
 ALTER TABLE master.blocklisted_words ALTER COLUMN lang_code DROP NOT NULL;
 ALTER TABLE master.blocklisted_words ADD CONSTRAINT pk_blwrd_code PRIMARY KEY (word);
+
+INSERT into master.blocklisted_words (word,descr,lang_code,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes,is_deleted,del_dtimes) SELECT word,descr,lang_code,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes,is_deleted,del_dtimes FROM master.blacklisted_words;
+
 ALTER TABLE master.batch_job_execution_params ALTER COLUMN string_val TYPE varchar(5000) USING string_val::varchar;
 
 ALTER TABLE master.user_detail DROP COLUMN uin;
@@ -254,6 +257,19 @@ ALTER TABLE master.user_detail DROP COLUMN mobile;
 ALTER TABLE master.user_detail_h DROP COLUMN uin;
 ALTER TABLE master.user_detail_h DROP COLUMN email;
 ALTER TABLE master.user_detail_h DROP COLUMN mobile;
+
+SELECT * INTO master.template_copy
+FROM master.template;
+
+DELETE *
+	FROM master.template where template_typ_code not
+like 'reg-%' and module_id='10002';
+
+UPDATE master.template set module_id='10002' where template_typ_code like 'reg-ack%';
+
+UPDATE master.template set module_id='10002' where template_typ_code like 'reg-preview%';
+
+UPDATE master.template set module_id='10002' where template_typ_code like 'reg-dashboard%';
 
 -----------------------------------------------------------------------------------------------------------------------------------------------
 -------------------------------------template,template_type and module_detail----------------------------------------------------------
