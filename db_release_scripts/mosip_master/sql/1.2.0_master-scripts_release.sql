904668ac81be3e948822c585ebd53cf8dd0aa546
ase-101 <sunkadaeanusha@gmail.com>
2022-07-20 17:31:56 +0530

    updated migration scripts with migration fixes from develop to 1.2.0.1

--- db_release_scripts/mosip_master/sql/1.2.0_master-scripts_release.sql
+++ db_release_scripts/mosip_master/sql/1.2.0_master-scripts_release.sql
@@ -40,8 +40,9 @@ ALTER TABLE master.blacklisted_words DROP CONSTRAINT IF EXISTS pk_blwrd_code CAS
 \ir ../ddl/master-blocklisted_words.sql
 ALTER TABLE master.blocklisted_words DROP CONSTRAINT IF EXISTS pk_blwrd_code CASCADE;
 ALTER TABLE master.blocklisted_words ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.blocklisted_words ADD CONSTRAINT pk_blwrd_code PRIMARY KEY (word);
 INSERT into master.blocklisted_words (word,descr,lang_code,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes,is_deleted,del_dtimes) SELECT distinct word,descr,lang_code,is_active,cr_by,cr_dtimes,upd_by,upd_dtimes,is_deleted,del_dtimes FROM master.blacklisted_words;
+DELETE FROM master.blocklisted_words where lang_code != :primary_language_code;
+ALTER TABLE master.blocklisted_words ADD CONSTRAINT pk_blwrd_code PRIMARY KEY (word);
 DROP TABLE IF EXISTS master.blacklisted_words;
 
 -------------------------------------------UI SPEC TABLE ----------------------------------------------
@@ -61,102 +62,20 @@ ALTER TABLE master.bulkupload_transaction ALTER COLUMN upload_description TYPE c
 
 -----------------------------------------------ALTER FK constraints with lang code -----------------------------------------------------------
 
-ALTER TABLE master.device_master DROP CONSTRAINT IF EXISTS fk_devicem_dspec CASCADE;
-ALTER TABLE master.device_master ADD CONSTRAINT fk_devicem_dspec FOREIGN KEY (dspec_id)
-REFERENCES master.device_spec (id) MATCH SIMPLE
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-ALTER TABLE master.device_master DROP CONSTRAINT IF EXISTS fk_devicem_zone CASCADE;
-ALTER TABLE master.device_master ADD CONSTRAINT fk_devicem_zone FOREIGN KEY (zone_code)
-REFERENCES master.zone (code) MATCH FULL
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-ALTER TABLE master.device_master DROP CONSTRAINT IF EXISTS fk_devicem_center CASCADE;
-ALTER TABLE master.device_master ADD CONSTRAINT fk_devicem_center FOREIGN KEY (regcntr_id)
-REFERENCES master.registration_center (id) MATCH SIMPLE
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-ALTER TABLE master.device_spec DROP CONSTRAINT IF EXISTS fk_dspec_dtyp CASCADE;
-ALTER TABLE master.device_spec ADD CONSTRAINT fk_dspec_dtyp FOREIGN KEY (dtyp_code)
-REFERENCES master.device_type (code) MATCH SIMPLE
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-
-
-ALTER TABLE master.machine_master DROP CONSTRAINT IF EXISTS fk_machm_mspec CASCADE;
-ALTER TABLE master.machine_master ADD CONSTRAINT fk_machm_mspec FOREIGN KEY (mspec_id)
-REFERENCES master.machine_spec (id) MATCH SIMPLE
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-
-ALTER TABLE master.machine_master DROP CONSTRAINT IF EXISTS fk_machm_zone CASCADE;
-ALTER TABLE master.machine_master ADD CONSTRAINT fk_machm_zone FOREIGN KEY (zone_code)
-REFERENCES master.zone (code) MATCH FULL
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-
-ALTER TABLE master.machine_master DROP CONSTRAINT IF EXISTS fk_machm_center CASCADE;
-ALTER TABLE master.machine_master ADD CONSTRAINT fk_machm_center FOREIGN KEY (regcntr_id)
-REFERENCES master.registration_center (id) MATCH SIMPLE
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-
-
-ALTER TABLE master.machine_spec DROP CONSTRAINT IF EXISTS fk_mspec_mtyp CASCADE;
-ALTER TABLE master.machine_spec ADD CONSTRAINT fk_mspec_mtyp FOREIGN KEY (mtyp_code)
-REFERENCES master.machine_type (code) MATCH SIMPLE
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
+ALTER TABLE master.biometric_type DROP CONSTRAINT IF EXISTS pk_bmtyp_code CASCADE;
+ALTER TABLE master.biometric_type ALTER COLUMN lang_code DROP NOT NULL;
+DELETE FROM master.biometric_type where lang_code != :primary_language_code;
+ALTER TABLE master.biometric_type ADD CONSTRAINT pk_bmtyp_code PRIMARY KEY (code);
 
 ALTER TABLE master.biometric_attribute DROP CONSTRAINT IF EXISTS fk_bmattr_bmtyp CASCADE;
 ALTER TABLE master.biometric_attribute ADD CONSTRAINT fk_bmattr_bmtyp FOREIGN KEY (bmtyp_code)
 REFERENCES master.biometric_type (code) MATCH SIMPLE
 ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE master.zone_user DROP CONSTRAINT IF EXISTS fk_zoneuser_zone CASCADE;
-ALTER TABLE master.zone_user ADD CONSTRAINT fk_zoneuser_zone FOREIGN KEY (zone_code)
-REFERENCES master.zone (code) MATCH FULL
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-ALTER TABLE master.reg_working_nonworking DROP CONSTRAINT IF EXISTS fk_rwn_daycode CASCADE;
-ALTER TABLE master.reg_working_nonworking ADD CONSTRAINT fk_rwn_daycode FOREIGN KEY (day_code)
-REFERENCES master.daysofweek_list (code) MATCH FULL
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-
-ALTER TABLE master.reg_working_nonworking DROP CONSTRAINT IF EXISTS fk_rwn_regcntr CASCADE;
-ALTER TABLE master.reg_working_nonworking ADD CONSTRAINT fk_rwn_regcntr FOREIGN KEY (regcntr_id)
-REFERENCES master.registration_center (id) MATCH FULL
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-ALTER TABLE master.reg_exceptional_holiday DROP CONSTRAINT IF EXISTS fk_regeh_regcntr CASCADE;
-ALTER TABLE master.reg_exceptional_holiday ADD CONSTRAINT fk_regeh_regcntr FOREIGN KEY (regcntr_id)
-REFERENCES master.registration_center (id) MATCH FULL
-ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-ALTER TABLE master.device_master_h DROP CONSTRAINT IF EXISTS pk_devicem_h_id CASCADE;
-ALTER TABLE master.device_master_h ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.device_master_h ADD CONSTRAINT pk_devicem_h_id PRIMARY KEY (id,eff_dtimes);
-
-ALTER TABLE master.machine_master_h DROP CONSTRAINT IF EXISTS pk_machm_h_id CASCADE;
-ALTER TABLE master.machine_master_h ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.machine_master_h ADD CONSTRAINT pk_machm_h_id PRIMARY KEY (id,eff_dtimes);
 
 ALTER TABLE master.user_detail_h DROP CONSTRAINT IF EXISTS pk_usrdtl_h_id CASCADE;
 ALTER TABLE master.user_detail_h ALTER COLUMN lang_code DROP NOT NULL;
 ALTER TABLE master.user_detail_h ADD CONSTRAINT pk_usrdtl_h_id PRIMARY KEY (id,eff_dtimes);
-
 
 --------------------------------------------------------------DROP CONSTRAINTS-------------------------------------------
 
@@ -168,11 +87,11 @@ ALTER TABLE master.user_detail ALTER COLUMN lang_code DROP NOT NULL;
 
 ALTER TABLE master.user_detail DROP CONSTRAINT IF EXISTS fk_usrdtl_center CASCADE;
 ALTER TABLE master.zone_user DROP CONSTRAINT IF EXISTS fk_zoneuser_zone CASCADE;
-
+ALTER TABLE master.template DROP CONSTRAINT IF EXISTS fk_tmplt_moddtl CASCADE;
 
 --------------------------------------------LANG CODE NULLABLE AND CHANGE PK CONSTRAINTS ---------------------------------
 
-ALTER TABLE master.app_authentication_method DROP CONSTRAINT IF EXISTS pk_appauthm_id;
+ALTER TABLE master.app_authentication_method DROP CONSTRAINT IF EXISTS pk_appauthm_id CASCADE;
 ALTER TABLE master.app_authentication_method ALTER COLUMN lang_code DROP NOT NULL;
 ALTER TABLE master.app_authentication_method ADD CONSTRAINT pk_appauthm_id PRIMARY KEY (app_id,process_id,role_code,auth_method_code);
 
@@ -181,67 +100,76 @@ ALTER TABLE master.app_role_priority DROP CONSTRAINT IF EXISTS pk_roleprt_id;
 ALTER TABLE master.app_role_priority ALTER COLUMN lang_code DROP NOT NULL;
 ALTER TABLE master.app_role_priority ADD CONSTRAINT pk_roleprt_id PRIMARY KEY (app_id,process_id,role_code);
 
-ALTER TABLE master.applicant_valid_document DROP CONSTRAINT IF EXISTS pk_avaldoc_code;
-ALTER TABLE master.applicant_valid_document ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.applicant_valid_document ADD CONSTRAINT pk_avaldoc_code PRIMARY KEY (apptyp_code,doccat_code,doctyp_code);
 
-ALTER TABLE master.authentication_method DROP CONSTRAINT IF EXISTS pk_authm_code;
-ALTER TABLE master.authentication_method ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.authentication_method ADD CONSTRAINT pk_authm_code PRIMARY KEY (code);
-
-
-ALTER TABLE master.biometric_type DROP CONSTRAINT IF EXISTS pk_bmtyp_code;
-ALTER TABLE master.biometric_type ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.biometric_type ADD CONSTRAINT pk_bmtyp_code PRIMARY KEY (code);
-
-
-ALTER TABLE master.device_master DROP CONSTRAINT IF EXISTS pk_devicem_id;
-ALTER TABLE master.device_master ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.device_master ADD CONSTRAINT pk_devicem_id PRIMARY KEY (id);
-
-
+ALTER TABLE master.device_master DROP CONSTRAINT IF EXISTS fk_devicem_zone CASCADE;
+ALTER TABLE master.device_master DROP CONSTRAINT IF EXISTS fk_devicem_dspec CASCADE;
+ALTER TABLE master.device_spec DROP CONSTRAINT IF EXISTS fk_dspec_dtyp CASCADE;
+ALTER TABLE master.device_type DROP CONSTRAINT IF EXISTS pk_dtyp_code;
 ALTER TABLE master.device_spec DROP CONSTRAINT IF EXISTS pk_dspec_code;
+ALTER TABLE master.device_master DROP CONSTRAINT IF EXISTS pk_devicem_id;
+ALTER TABLE master.device_type ALTER COLUMN lang_code DROP NOT NULL;
 ALTER TABLE master.device_spec ALTER COLUMN lang_code DROP NOT NULL;
+ALTER TABLE master.device_master ALTER COLUMN lang_code DROP NOT NULL;
+DELETE FROM master.device_type where lang_code != :primary_language_code;
+DELETE FROM master.device_spec where lang_code != :primary_language_code;
+ALTER TABLE master.device_type ADD CONSTRAINT pk_dtyp_code PRIMARY KEY (code);
 ALTER TABLE master.device_spec ADD CONSTRAINT pk_dspec_code PRIMARY KEY (id);
+ALTER TABLE master.device_spec ADD CONSTRAINT fk_dspec_dtyp FOREIGN KEY (dtyp_code)
+REFERENCES master.device_type (code) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+DELETE FROM master.device_master where lang_code != :primary_language_code;
+ALTER TABLE master.device_master ADD CONSTRAINT pk_devicem_id PRIMARY KEY (id);
+ALTER TABLE master.device_master ADD CONSTRAINT fk_devicem_dspec FOREIGN KEY (dspec_id)
+REFERENCES master.device_spec (id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 
-ALTER TABLE master.device_type DROP CONSTRAINT IF EXISTS pk_dtyp_code;
-ALTER TABLE master.device_type ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.device_type ADD CONSTRAINT pk_dtyp_code PRIMARY KEY (code);
-
-ALTER TABLE master.device_type DROP CONSTRAINT IF EXISTS pk_dtyp_code;
-ALTER TABLE master.device_type ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.device_type ADD CONSTRAINT pk_dtyp_code PRIMARY KEY (code);
-
-ALTER TABLE master.machine_master DROP CONSTRAINT IF EXISTS pk_machm_id;
-ALTER TABLE master.machine_master ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.machine_master ADD CONSTRAINT pk_machm_id PRIMARY KEY (id);
+DELETE FROM master.device_master_h where lang_code != :primary_language_code;
+ALTER TABLE master.device_master_h DROP CONSTRAINT IF EXISTS pk_devicem_h_id CASCADE;
+ALTER TABLE master.device_master_h ALTER COLUMN lang_code DROP NOT NULL;
+ALTER TABLE master.device_master_h ADD CONSTRAINT pk_devicem_h_id PRIMARY KEY (id,eff_dtimes);
 
 
-ALTER TABLE master.machine_spec DROP CONSTRAINT IF EXISTS pk_mspec_code;
-ALTER TABLE master.machine_spec ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.machine_spec ADD CONSTRAINT pk_mspec_code PRIMARY KEY (id);
-
-
+ALTER TABLE master.machine_master DROP CONSTRAINT IF EXISTS fk_machm_mspec CASCADE;
+ALTER TABLE master.machine_master DROP CONSTRAINT IF EXISTS fk_machm_zone CASCADE;
+ALTER TABLE master.machine_master DROP CONSTRAINT IF EXISTS fk_machm_center CASCADE;
+ALTER TABLE master.machine_spec DROP CONSTRAINT IF EXISTS fk_mspec_mtyp CASCADE;
 ALTER TABLE master.machine_type DROP CONSTRAINT IF EXISTS pk_mtyp_code;
+ALTER TABLE master.machine_spec DROP CONSTRAINT IF EXISTS pk_mspec_code;
+ALTER TABLE master.machine_master DROP CONSTRAINT IF EXISTS pk_machm_id;
 ALTER TABLE master.machine_type ALTER COLUMN lang_code DROP NOT NULL;
+ALTER TABLE master.machine_master ALTER COLUMN lang_code DROP NOT NULL;
+ALTER TABLE master.machine_spec ALTER COLUMN lang_code DROP NOT NULL;
+DELETE FROM master.machine_type where lang_code != :primary_language_code;
+DELETE FROM master.machine_spec where lang_code != :primary_language_code;
+DELETE FROM master.machine_master where lang_code != :primary_language_code;
 ALTER TABLE master.machine_type ADD CONSTRAINT pk_mtyp_code PRIMARY KEY (code);
+ALTER TABLE master.machine_spec ADD CONSTRAINT pk_mspec_code PRIMARY KEY (id);
+ALTER TABLE master.machine_master ADD CONSTRAINT pk_machm_id PRIMARY KEY (id);
+ALTER TABLE master.machine_spec ADD CONSTRAINT fk_mspec_mtyp FOREIGN KEY (mtyp_code)
+REFERENCES master.machine_type (code) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE master.machine_master ADD CONSTRAINT fk_machm_mspec FOREIGN KEY (mspec_id)
+REFERENCES master.machine_spec (id) MATCH SIMPLE
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+DELETE FROM master.machine_master_h where lang_code != :primary_language_code;
+ALTER TABLE master.machine_master_h DROP CONSTRAINT IF EXISTS pk_machm_h_id CASCADE;
+ALTER TABLE master.machine_master_h ALTER COLUMN lang_code DROP NOT NULL;
+ALTER TABLE master.machine_master_h ADD CONSTRAINT pk_machm_h_id PRIMARY KEY (id,eff_dtimes);
 
 
+
+ALTER TABLE master.reg_exceptional_holiday DROP CONSTRAINT IF EXISTS fk_regeh_regcntr CASCADE;
 ALTER TABLE master.reg_exceptional_holiday DROP CONSTRAINT IF EXISTS pk_exceptional_hol;
+DELETE FROM master.reg_exceptional_holiday where lang_code != :primary_language_code;
 ALTER TABLE master.reg_exceptional_holiday ALTER COLUMN lang_code DROP NOT NULL;
 ALTER TABLE master.reg_exceptional_holiday ADD CONSTRAINT pk_exceptional_hol PRIMARY KEY (regcntr_id,hol_date);
 
 
-ALTER TABLE master.reg_working_nonworking DROP CONSTRAINT IF EXISTS pk_working_nonworking;
-ALTER TABLE master.reg_working_nonworking ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.reg_working_nonworking ADD CONSTRAINT pk_working_nonworking PRIMARY KEY (regcntr_id,day_code);
 
-
-ALTER TABLE master.zone_user DROP CONSTRAINT IF EXISTS pk_tmpltyp_code;
 ALTER TABLE master.zone_user ALTER COLUMN lang_code DROP NOT NULL;
 ALTER TABLE master.user_detail_h ALTER COLUMN status_code DROP NOT NULL;
-ALTER TABLE master.zone_user ADD CONSTRAINT pk_zoneuser PRIMARY KEY (usr_id);
 ALTER TABLE master.machine_master ADD CONSTRAINT uq_machm_name UNIQUE (name);
 ALTER TABLE master.machine_master ADD CONSTRAINT uq_machm_key_index UNIQUE (key_index);
 ALTER TABLE master.machine_master ADD CONSTRAINT uq_machm_skey_index UNIQUE (sign_key_index);
@@ -250,7 +178,6 @@ ALTER TABLE master.machine_master ADD CONSTRAINT uq_machm_skey_index UNIQUE (sig
 ALTER TABLE master.loc_holiday DROP CONSTRAINT IF EXISTS pk_lochol_id;
 ALTER TABLE master.loc_holiday ADD CONSTRAINT pk_lochol_id PRIMARY KEY (holiday_date, location_code, lang_code);
 
-ALTER TABLE master.batch_job_execution_params ALTER COLUMN string_val TYPE varchar(5000) USING string_val::varchar;
 ALTER TABLE master.batch_job_execution_params ALTER COLUMN string_val TYPE varchar(5000) USING string_val::varchar;
 
 ALTER TABLE master.user_detail DROP COLUMN uin;
