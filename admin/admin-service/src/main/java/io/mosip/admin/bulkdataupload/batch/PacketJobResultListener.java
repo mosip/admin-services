315fe653d26aa9b0ae52a7735cf60289833db7e0
Mohammed ZAHID <zahid.med@gmail.com>
2023-02-22 08:57:45 +0100

    Bump spring-batch-context from 1.0.0 to 1.1.0-RELEASE

--- admin/admin-service/src/main/java/io/mosip/admin/bulkdataupload/batch/PacketJobResultListener.java
+++ admin/admin-service/src/main/java/io/mosip/admin/bulkdataupload/batch/PacketJobResultListener.java
@@ -2,20 +2,14 @@ package io.mosip.admin.bulkdataupload.batch;
 import io.mosip.admin.bulkdataupload.repositories.BulkUploadTranscationRepository;
 import io.mosip.admin.packetstatusupdater.util.AuditUtil;
 import io.mosip.admin.packetstatusupdater.util.EventEnum;
-import org.digibooster.spring.batch.security.listener.JobExecutionSecurityContextListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.item.file.FlatFileParseException;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 
-import javax.sql.DataSource;
-import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -24,31 +18,18 @@ public class PacketJobResultListener implements JobExecutionListener {
 	 private static final Logger logger = LoggerFactory.getLogger(JobResultListener.class);
 	    private static String STATUS_MESSAGE = " <br/> READ: %d, STATUS: %s, MESSAGE: %s";
 	    private AuditUtil auditUtil;
-	    private JobExecutionSecurityContextListener jobExecutionSecurityContextListener;
 	    private BulkUploadTranscationRepository bulkUploadTranscationRepository;
 	    private static final String SECURITY_PARAM_NAME = "security-param";
 	    private static final ThreadLocal<Authentication> ORIGINAL_CONTEXT = new ThreadLocal<>();
 
 	    public PacketJobResultListener(BulkUploadTranscationRepository bulkUploadTranscationRepository,
-	                             AuditUtil auditUtil, JobExecutionSecurityContextListener jobExecutionSecurityContextListener) {
+	                             AuditUtil auditUtil) {
 	        this.bulkUploadTranscationRepository = bulkUploadTranscationRepository;
 	        this.auditUtil = auditUtil;
-	        this.jobExecutionSecurityContextListener = new JobExecutionSecurityContextListener();
 	    }
 
 	    @Override
 	    public void beforeJob(JobExecution jobExecution) {
-	        logger.info("Job started : {}", jobExecution.getJobParameters().getString("transactionId"));
-	        this.jobExecutionSecurityContextListener.fillJobExecutionContext(jobExecution);
-
-	        if(jobExecution.getStepExecutions().isEmpty()) {
-	            restoreContext(jobExecution);
-	        }
-	        else {
-	            for(StepExecution stepExecution : jobExecution.getStepExecutions()) {
-	                this.jobExecutionSecurityContextListener.restoreContext(stepExecution);
-	            }
-	        }
 	    }
 
 	    @Override
@@ -80,33 +61,7 @@ public class PacketJobResultListener implements JobExecutionListener {
 
 	        } catch (Throwable t) {
 	            logger.error("Failed  to update job status {}", jobId, t);
-	        } finally {
-	            clearContext();
-	            this.jobExecutionSecurityContextListener.removeFromJobExecutionContext(jobExecution);
 	        }
 	    }
 
-
-	    private void restoreContext(JobExecution jobExecution) {
-	        if (jobExecution.getExecutionContext().containsKey(SECURITY_PARAM_NAME)) {
-	            logger.debug("Restore the security context");
-	            Authentication authentication = (Authentication) jobExecution.getExecutionContext()
-	                    .get(SECURITY_PARAM_NAME);
-	            SecurityContext securityContext = SecurityContextHolder.getContext();
-	            ORIGINAL_CONTEXT.set(securityContext.getAuthentication());
-	            securityContext.setAuthentication(authentication);
-	        } else {
-	            logger.error("Could not find key {} in the job execution context", SECURITY_PARAM_NAME);
-	        }
-	    }
-
-	    private void clearContext() {
-	        logger.debug("Clear the security context");
-	        SecurityContextHolder.clearContext();
-	        Authentication originalAuth = ORIGINAL_CONTEXT.get();
-	        if (originalAuth != null) {
-	            SecurityContextHolder.getContext().setAuthentication(originalAuth);
-	            ORIGINAL_CONTEXT.remove();
-	        }
-	    }
 }
