7a0aa7f2a8527c5ee3837fdef4bbb348c8ea1e69
Balaji <74903654+balaji-alluru@users.noreply.github.com>
2023-04-10 03:04:56 +0530

    MOSIP-25760 : removed blacklisted words table.
    Signed-off-by: Balaji <74903654+balaji-alluru@users.noreply.github.com>

---
+++ db_release_scripts/mosip_master/sql/1.2.1-SNAPSHOT_to_1.2.0.1-B1_revoke.sql
@@ -0,0 +1,11 @@
+-- --------------------------------------------------------------------------------------------------------
+-- Database Name: mosip_master
+-- Release Version 	: 1.2.1
+-- Purpose    		: Revoking Database Alter deployement done for release in Master DB.       
+-- Create By   		: Balaji A
+-- Created Date		: April 2023
+-- 
+-- Modified Date        Modified By         Comments / Remarks
+-- -----------------------------------------------------------------------------------------------------------
+
+-- -----------------------------------------------------------------------------------------------------------
