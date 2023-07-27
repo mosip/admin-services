1a83ec0ffacfa6eea8529c1edcb4335979a045a7
dhanendra06 <60607841+dhanendra06@users.noreply.github.com>
2023-07-27 11:30:27 +0530

    MOSIP-12430 (#887)
    * MOSIP-12430
    
    * modified code based on review comments
    
    * updated code based on review comments
    
    * modified the code based on review comments
    
    * modified the code based on review comments
    
    * modified the code based on review comments
    
    * modified the code based on review comments
    
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>

---
+++ admin/admin-service/src/main/java/io/mosip/admin/dto/SearchFieldResponseDto.java
@@ -0,0 +1,17 @@
+package io.mosip.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchFieldResponseDto {
+
+    Map<String, String> fields;
+}
