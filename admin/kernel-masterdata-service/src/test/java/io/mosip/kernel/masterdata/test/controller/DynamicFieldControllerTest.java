e03ca0e0d46503468248947b566dbd30d7aa1218
kameshsr <kameshsr1338@gmail.com>
2023-09-21 14:31:32 +0530

    MOSIP-29472 Changed logic as per review comment.

--- admin/kernel-masterdata-service/src/test/java/io/mosip/kernel/masterdata/test/controller/DynamicFieldControllerTest.java
+++ admin/kernel-masterdata-service/src/test/java/io/mosip/kernel/masterdata/test/controller/DynamicFieldControllerTest.java
@@ -421,7 +421,7 @@ public class DynamicFieldControllerTest {
 	@WithUserDetails("global-admin")
 	public void t000getAllDynamicFieldByNameTest() throws Exception {
 		MasterDataTest.checkResponse(
-				mockMvc.perform(MockMvcRequestBuilders.get("/dynamicfields/all/gender").param("withValue", "true")).andReturn(),
+				mockMvc.perform(MockMvcRequestBuilders.get("/dynamicfields/all/gender")).andReturn(),
 				null);
 	}
 
