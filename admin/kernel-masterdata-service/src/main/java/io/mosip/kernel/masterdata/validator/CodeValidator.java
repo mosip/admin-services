74dfbd9a1c16c4f8e141805a9d145ac3b3546472
SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>
2024-01-03 16:53:51 +0530

    MOSIP-20534: Added validators for code and value fields of dynamic field
    Signed-off-by: SwethaKrish4 <110164892+SwethaKrish4@users.noreply.github.com>

---
+++ admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/validator/CodeValidator.java
@@ -0,0 +1,29 @@
+package io.mosip.kernel.masterdata.validator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Data
+public class CodeValidator implements ConstraintValidator<DynamicCodeValidator,JsonNode> {
+    @Value("${mosip.kernel.masterdata.code.validate.regex}")
+    private String allowedCharactersRegex;
+
+
+    @Override
+    public boolean isValid(JsonNode value, ConstraintValidatorContext context) {
+        JsonNode val=value.get("code");
+        String code= val.asText();
+        if(code!=null && !code.isEmpty()){
+            Pattern p= Pattern.compile(allowedCharactersRegex,Pattern.CASE_INSENSITIVE);
+            Matcher m=p.matcher(code.trim());
+            return !(m.find());
+        }
+        return true;
+    }
+}
