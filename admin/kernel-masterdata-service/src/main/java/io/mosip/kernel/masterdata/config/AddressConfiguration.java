f7b605eff9fdbcda246aae01ae6d68b6dd85b668
Monobikash Das <m1045447@mindtree.com>
2024-01-29 17:00:26 +0530

    Latest address search drools engine API
    Signed-off-by: Monobikash Das <monobikash.das@mindtree.com>

--- admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/config/AddressConfiguration.java
+++ admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/config/AddressConfiguration.java
@@ -44,29 +44,6 @@ public class AddressConfiguration {
     private List<String> droolsRuleFileName;
 
     @Bean
-    public RestTemplate restTemplate()
-            throws KeyStoreException, KeyManagementException, NoSuchAlgorithmException {
-        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
-
-        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build();
-
-        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setSSLSocketFactory(csf)
-                .build();
-
-        HttpComponentsClientHttpRequestFactory requestFactory =
-                new HttpComponentsClientHttpRequestFactory();
-
-        requestFactory.setHttpClient(httpClient);
-        RestTemplate restTemplate = new RestTemplate(requestFactory);
-        return restTemplate;
-    }
-
-    @Bean
     public KieSession kieSession() throws IOException {
         LOGGER.info("Session created...");
         KieSession kieSession = getKieContainer().newKieSession();
