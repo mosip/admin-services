694297e04cea8bb62883f2fb0408f33f7e91acfc
dhanendra06 <dhanendra.tech@gmail.com>
2024-06-04 21:03:51 +0530

    MOSIP-32453: fixed delete query issue
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>

--- admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/ValidDocumentServiceImpl.java
+++ admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/service/impl/ValidDocumentServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import javax.transaction.Transactional;
+import jakarta.transaction.Transactional;
 
 import io.mosip.kernel.masterdata.dto.response.FilterResult;
 import org.springframework.beans.factory.annotation.Value;
