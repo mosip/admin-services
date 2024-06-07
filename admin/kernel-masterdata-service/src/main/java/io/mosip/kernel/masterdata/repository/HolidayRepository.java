6d3ac287ca6bcbc0df1d9756a1cf1d3797e69920
dhanendra06 <dhanendra.tech@gmail.com>
2024-06-07 13:02:12 +0530

    fixed registrationcenter creation issue
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>

--- admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/repository/HolidayRepository.java
+++ admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/repository/HolidayRepository.java
@@ -139,8 +139,9 @@ public interface HolidayRepository extends BaseRepository<Holiday, Integer> {
 	@Query("FROM Holiday where locationCode = ?1 and langCode=?2 and (isDeleted = false or isDeleted is null) and isActive = true")
 	List<Holiday> findHolidayByLocationCode(String locationCode, String langCode);
 
-	@Query(value = "select  holiday_date from master.loc_holiday WHERE location_code = ?1 and lang_code = ?2", nativeQuery = true)
-	List<LocalDate> findHolidayByLocationCode1(String locationCode, String langCode);
+	@Query(value = "SELECT holidayDate FROM Holiday WHERE locationCode = ?1 and langCode = ?2")
+	List<LocalDate> findHolidayByLocationCode1
+			(String locationCode, String langCode);
 	
 	@Query(value = "select A.* from (select distinct * from master.loc_holiday) A left join (select distinct holiday_date, lang_code from master.loc_holiday where lang_code=?1) B on A.holiday_date=B.holiday_date where B.holiday_date is null", nativeQuery = true)
 	List<Holiday> findHolidayForMissingData(String langCode);
