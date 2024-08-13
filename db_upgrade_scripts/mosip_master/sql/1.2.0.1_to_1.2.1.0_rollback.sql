757b5b15e732a5de967a8b98a298f4a5af845d1f
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2024-08-13 12:06:47 +0530

    Added the PK constraint with the lang_code column back
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- db_upgrade_scripts/mosip_master/sql/1.2.0.1_to_1.2.1.0_rollback.sql
+++ db_upgrade_scripts/mosip_master/sql/1.2.0.1_to_1.2.1.0_rollback.sql
@@ -1,15 +1,19 @@
 -- Rollback script for master.app_detail
 ALTER TABLE master.app_detail ALTER COLUMN lang_code SET NOT NULL;
 ALTER TABLE master.app_detail DROP CONSTRAINT pk_appdtl_id;
+ALTER TABLE master.app_detail ADD CONSTRAINT pk_appdtl_id PRIMARY KEY (id, lang_code);
 
 -- Rollback script for master.biometric_attribute
 ALTER TABLE master.biometric_attribute ALTER COLUMN lang_code SET NOT NULL;
 ALTER TABLE master.biometric_attribute DROP CONSTRAINT pk_bmattr_code;
+ALTER TABLE master.biometric_attribute ADD CONSTRAINT pk_bmattr_code PRIMARY KEY (code, lang_code);
 
 -- Rollback script for master.module_detail
 ALTER TABLE master.module_detail ALTER COLUMN lang_code SET NOT NULL;
 ALTER TABLE master.module_detail DROP CONSTRAINT pk_moddtl_code;
+ALTER TABLE master.module_detail ADD CONSTRAINT pk_moddtl_code PRIMARY KEY (id, lang_code);
 
 -- Rollback script for master.template_file_format
 ALTER TABLE master.template_file_format ALTER COLUMN lang_code SET NOT NULL;
 ALTER TABLE master.template_file_format DROP CONSTRAINT pk_tffmt_code;
+ALTER TABLE master.template_file_format ADD CONSTRAINT pk_tffmt_code PRIMARY KEY (code, lang_code);
