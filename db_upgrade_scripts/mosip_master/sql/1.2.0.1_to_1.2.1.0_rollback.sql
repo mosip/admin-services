909278c787c773009cd808e4d71584b3d7320338
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2024-08-20 10:50:13 +0530

    Revert "updated upgrade and rollback scripts"
    This reverts commit a8613c2a4032f5e6176e0fee87b3455aea155cb0.

--- db_upgrade_scripts/mosip_master/sql/1.2.0.1_to_1.2.1.0_rollback.sql
+++ db_upgrade_scripts/mosip_master/sql/1.2.0.1_to_1.2.1.0_rollback.sql
@@ -21,27 +21,3 @@ ALTER TABLE master.template_file_format ADD CONSTRAINT pk_tffmt_code PRIMARY KEY
 
 -- Rollback script for master.template
 ALTER TABLE master.template ADD CONSTRAINT fk_tmplt_tffmt FOREIGN KEY (file_format_code, lang_code) REFERENCES master.template_file_format (code, lang_code);
-
-
--------------------------------------------------------------------------------------------------------------------------------------------
-
-
--- ------------------------------------------------------------------------------------------
--- Revoke script for Migrating Spring batch version to 5.0 as part of Java 21 Migration.
--- ------------------------------------------------------------------------------------------
-ALTER TABLE master.BATCH_STEP_EXECUTION DROP COLUMN CREATE_TIME;
-ALTER TABLE master.BATCH_STEP_EXECUTION ALTER COLUMN START_TIME DROP NOT NULL;
-ALTER TABLE master.BATCH_JOB_EXECUTION_PARAMS ADD COLUMN DATE_VAL DATE;
-ALTER TABLE master.BATCH_JOB_EXECUTION_PARAMS ADD COLUMN LONG_VAL BIGINT;
-ALTER TABLE master.BATCH_JOB_EXECUTION_PARAMS ADD COLUMN DOUBLE_VAL DOUBLE PRECISION;
-ALTER TABLE master.BATCH_JOB_EXECUTION_PARAMS RENAME PARAMETER_TYPE TO TYPE_CD;
-ALTER TABLE master.BATCH_JOB_EXECUTION_PARAMS ALTER COLUMN TYPE_CD TYPE VARCHAR(6);
-ALTER TABLE master.BATCH_JOB_EXECUTION_PARAMS RENAME PARAMETER_NAME TO KEY_NAME;
-ALTER TABLE master.BATCH_JOB_EXECUTION_PARAMS ALTER COLUMN KEY_NAME TYPE VARCHAR(100);
-ALTER TABLE master.BATCH_JOB_EXECUTION_PARAMS RENAME PARAMETER_VALUE TO STRING_VAL;
-ALTER TABLE master.BATCH_JOB_EXECUTION_PARAMS ALTER COLUMN STRING_VAL TYPE VARCHAR(250);
-ALTER TABLE master.BATCH_JOB_EXECUTION ADD COLUMN JOB_CONFIGURATION_LOCATION VARCHAR(2500);
-
-DROP SEQUENCE master.BATCH_STEP_EXECUTION_SEQ;
-DROP SEQUENCE master.BATCH_JOB_EXECUTION_SEQ;
-DROP SEQUENCE master.BATCH_JOB_SEQ;
