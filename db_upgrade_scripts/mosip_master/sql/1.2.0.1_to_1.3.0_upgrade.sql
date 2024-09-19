5fa22c03ab79d8fa662864e1493215e1e191df90
dhanendra06 <60607841+dhanendra06@users.noreply.github.com>
2024-09-19 18:00:17 +0530

    MOSIP-32453 (#1077)
    * Added cleanup.sql
    
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>
    
    * removed whitespace
    
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>
    
    * taken a backup before deleting other lang data
    
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>
    
    ---------
    
    Signed-off-by: dhanendra06 <dhanendra.tech@gmail.com>

--- db_upgrade_scripts/mosip_master/sql/1.2.0.1_to_1.3.0_upgrade.sql
+++ db_upgrade_scripts/mosip_master/sql/1.2.0.1_to_1.3.0_upgrade.sql
@@ -1,21 +1,29 @@
 -- Upgrade script for master.app_detail
+SELECT * INTO master.app_detail_bkp FROM master.app_detail;
+DELETE FROM master.app_detail WHERE lang_code !='eng';
 ALTER TABLE master.app_detail DROP CONSTRAINT IF EXISTS pk_appdtl_id CASCADE;
 ALTER TABLE master.app_detail ALTER COLUMN lang_code DROP NOT NULL;
 ALTER TABLE master.app_detail ADD CONSTRAINT pk_appdtl_id PRIMARY KEY (id);
 
 -- Upgrade script for master.biometric_attribute
+SELECT * INTO master.biometric_attribute_bkp FROM master.biometric_attribute;
+DELETE FROM master.biometric_attribute WHERE lang_code !='eng';
 ALTER TABLE master.biometric_attribute DROP CONSTRAINT IF EXISTS pk_bmattr_code CASCADE;
 ALTER TABLE master.biometric_attribute ALTER COLUMN lang_code DROP NOT NULL;
 ALTER TABLE master.biometric_attribute ADD CONSTRAINT pk_bmattr_code PRIMARY KEY (code);
 
 -- Upgrade script for master.module_detail
+SELECT * INTO master.module_detail_bkp FROM master.module_detail;
+DELETE FROM master.module_detail WHERE lang_code !='eng';
 ALTER TABLE master.module_detail DROP CONSTRAINT IF EXISTS pk_moddtl_code CASCADE;
 ALTER TABLE master.module_detail ALTER COLUMN lang_code DROP NOT NULL;
 ALTER TABLE master.module_detail ADD CONSTRAINT pk_moddtl_code PRIMARY KEY (id);
 
 -- Upgrade script for master.template_file_format
+SELECT * INTO master.template_file_format_bkp FROM master.template_file_format;
 ALTER TABLE master.template DROP CONSTRAINT IF EXISTS fk_tmplt_tffmt CASCADE;
 ALTER TABLE master.template_file_format DROP CONSTRAINT IF EXISTS pk_tffmt_code CASCADE;
+DELETE FROM master.template_file_format WHERE lang_code !='eng';
 ALTER TABLE master.template_file_format ALTER COLUMN lang_code DROP NOT NULL;
 ALTER TABLE master.template_file_format ADD CONSTRAINT pk_tffmt_code PRIMARY KEY (code);
 
