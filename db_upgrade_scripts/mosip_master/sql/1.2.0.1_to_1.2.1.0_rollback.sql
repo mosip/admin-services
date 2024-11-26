7d6436d5e3e29ccd6ed17c6665928f0d4dfe45c3
kameshsr <kameshsr1338@gmail.com>
2024-11-26 18:03:13 +0530

    MOSIP-35902 Corrected upgrade script
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- db_upgrade_scripts/mosip_master/sql/1.2.0.1_to_1.2.1.0_rollback.sql
+++ db_upgrade_scripts/mosip_master/sql/1.2.0.1_to_1.2.1.0_rollback.sql
@@ -1,18 +1,3 @@
--- Rollback script for master.app_detail
-ALTER TABLE master.app_detail ALTER COLUMN lang_code SET NOT NULL;
-ALTER TABLE master.app_detail DROP CONSTRAINT pk_appdtl_id;
-ALTER TABLE master.app_detail ADD CONSTRAINT pk_appdtl_id PRIMARY KEY (id, lang_code);
-
--- Rollback script for master.biometric_attribute
-ALTER TABLE master.biometric_attribute ALTER COLUMN lang_code SET NOT NULL;
-ALTER TABLE master.biometric_attribute DROP CONSTRAINT pk_bmattr_code;
-ALTER TABLE master.biometric_attribute ADD CONSTRAINT pk_bmattr_code PRIMARY KEY (code, lang_code);
-
--- Rollback script for master.module_detail
-ALTER TABLE master.module_detail ALTER COLUMN lang_code SET NOT NULL;
-ALTER TABLE master.module_detail DROP CONSTRAINT pk_moddtl_code;
-ALTER TABLE master.module_detail ADD CONSTRAINT pk_moddtl_code PRIMARY KEY (id, lang_code);
-
 -- Rollback script for master.template_file_format
 ALTER TABLE master.template DROP CONSTRAINT IF EXISTS fk_tmplt_tffmt CASCADE;
 ALTER TABLE master.template_file_format ALTER COLUMN lang_code SET NOT NULL;
--- db_upgrade_scripts/mosip_master/sql/1.2.0.1_to_1.2.1.0_upgrade.sql
+++ db_upgrade_scripts/mosip_master/sql/1.2.0.1_to_1.2.1.0_upgrade.sql
@@ -1,18 +1,3 @@
--- Upgrade script for master.app_detail
-ALTER TABLE master.app_detail DROP CONSTRAINT IF EXISTS pk_appdtl_id CASCADE;
-ALTER TABLE master.app_detail ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.app_detail ADD CONSTRAINT pk_appdtl_id PRIMARY KEY (id);
-
--- Upgrade script for master.biometric_attribute
-ALTER TABLE master.biometric_attribute DROP CONSTRAINT IF EXISTS pk_bmattr_code CASCADE;
-ALTER TABLE master.biometric_attribute ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.biometric_attribute ADD CONSTRAINT pk_bmattr_code PRIMARY KEY (code);
-
--- Upgrade script for master.module_detail
-ALTER TABLE master.module_detail DROP CONSTRAINT IF EXISTS pk_moddtl_code CASCADE;
-ALTER TABLE master.module_detail ALTER COLUMN lang_code DROP NOT NULL;
-ALTER TABLE master.module_detail ADD CONSTRAINT pk_moddtl_code PRIMARY KEY (id);
-
 -- Upgrade script for master.template_file_format
 ALTER TABLE master.template DROP CONSTRAINT IF EXISTS fk_tmplt_tffmt CASCADE;
 ALTER TABLE master.template_file_format DROP CONSTRAINT IF EXISTS pk_tffmt_code CASCADE;
