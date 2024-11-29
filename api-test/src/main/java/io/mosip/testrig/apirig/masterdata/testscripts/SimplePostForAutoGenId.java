3e8c3e4b3f619bdf656d33404f63fa1441ed86a4
Mohanachandran S <mohanachandran.s@technoforte.co.in>
2024-11-29 14:31:18 +0530

    MOSIP-37943 - Fixed an automation failure
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>
    Signed-off-by: Balaji <74903654+balaji-alluru@users.noreply.github.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/masterdata/testscripts/SimplePostForAutoGenId.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/masterdata/testscripts/SimplePostForAutoGenId.java
@@ -91,10 +91,6 @@ public class SimplePostForAutoGenId extends AdminTestUtil implements ITest {
 					GlobalConstants.TARGET_ENV_HEALTH_CHECK_FAILED + HealthChecker.healthCheckFailureMapS);
 		}
 		
-		if (testCaseName.contains("UserCenterMapping_")) {
-			System.out.print("Done");
-		}
-		
 		String[] templateFields = testCaseDTO.getTemplateFields();
 		String inputJson = "";
 
