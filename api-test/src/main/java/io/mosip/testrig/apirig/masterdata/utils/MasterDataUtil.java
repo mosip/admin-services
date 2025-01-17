535e826fa7ad707ce53ca1e820a46aa70bbdf04c
Mohanachandran S <mohanachandran.s@technoforte.co.in>
2025-01-17 15:36:49 +0530

    MOSIP-38871 - Fixed the DOB hardcording for infants in getApplicantType api
    Signed-off-by: Mohanachandran S <mohanachandran.s@technoforte.co.in>

--- api-test/src/main/java/io/mosip/testrig/apirig/masterdata/utils/MasterDataUtil.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/masterdata/utils/MasterDataUtil.java
@@ -1,5 +1,8 @@
 package io.mosip.testrig.apirig.masterdata.utils;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.apache.log4j.Logger;
 import org.testng.SkipException;
 
@@ -11,6 +14,8 @@ import io.mosip.testrig.apirig.utils.SkipTestCaseHandler;
 public class MasterDataUtil extends AdminTestUtil {
 
 	private static final Logger logger = Logger.getLogger(MasterDataUtil.class);
+	public static DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+	public static String infantDob = LocalDateTime.now().minusYears(5).format(dateFormatter);
 	
 	public static String isTestCaseValidForExecution(TestCaseDTO testCaseDTO) {
 		String testCaseName = testCaseDTO.getTestCaseName();
@@ -19,6 +24,15 @@ public class MasterDataUtil extends AdminTestUtil {
 			throw new SkipException(GlobalConstants.KNOWN_ISSUES);
 		}
 		return testCaseName;
+	}
+	
+	public String inputJsonKeyWordHandeler(String jsonString, String testCaseName) {
+		
+		if (jsonString.contains("$INFANT$")) {
+			jsonString = replaceKeywordWithValue(jsonString, "$INFANT$", infantDob);
+		}
+		
+		return jsonString;
 	}
 	
 }
