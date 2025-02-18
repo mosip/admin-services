2aacb1c0a5b2c39bcbd59cd98179bf6d584f9f49
nagendra0721 <nagendra0718@gmail.com>
2025-02-18 11:29:47 +0530

    MOSIP-39775: admin-ca_cert_store-db-upgrade script
    Signed-off-by: nagendra0721 <nagendra0718@gmail.com>

--- db_upgrade_scripts/mosip_master/sql/1.2.1.1_to_1.3.0_rollback.sql
+++
@@ -1,8 +0,0 @@
-\c mosip_master
-
-COMMENT ON COLUMN master.ca_cert_store.ca_cert_type
-    IS NULL;
-
---Drop ca_cert_type Column (if it exists)--
-ALTER TABLE IF EXISTS master.ca_cert_store
-    DROP COLUMN IF EXISTS ca_cert_type;
