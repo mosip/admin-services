0bba3224eb50aa0fc24a77f3aef8be8abf0ada3d
Nandhukumar <nandhukumare@gmail.com>
2025-02-27 18:44:22 +0530

    MOSIP-39047
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>

--- api-test/src/main/java/io/mosip/testrig/apirig/masterdata/utils/MasterDataUtil.java
+++ api-test/src/main/java/io/mosip/testrig/apirig/masterdata/utils/MasterDataUtil.java
@@ -4,8 +4,12 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
+import javax.ws.rs.core.MediaType;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.SkipException;
 
 import com.google.gson.Gson;
@@ -13,8 +17,11 @@ import com.google.gson.reflect.TypeToken;
 
 import io.mosip.testrig.apirig.dto.TestCaseDTO;
 import io.mosip.testrig.apirig.utils.AdminTestUtil;
+import io.mosip.testrig.apirig.utils.ConfigManager;
 import io.mosip.testrig.apirig.utils.GlobalConstants;
+import io.mosip.testrig.apirig.utils.RestClient;
 import io.mosip.testrig.apirig.utils.SkipTestCaseHandler;
+import io.restassured.response.Response;
 
 public class MasterDataUtil extends AdminTestUtil {
 
@@ -64,6 +71,44 @@ public class MasterDataUtil extends AdminTestUtil {
 
 		String[] parts = infantAgeGroup.split("-");
 		return Integer.parseInt(parts[1]);
+	}
+	
+	public static JSONArray regprocActuatorResponseArray = null;
+
+	public static String getValueFromRegprocActuator(String section, String key) {
+		String url = ApplnURI + ConfigManager.getproperty("actuatorRegprocEndpoint");
+		String actuatorCacheKey = url + section + key;
+		String value = actuatorValueCache.get(actuatorCacheKey);
+		if (value != null && !value.isEmpty())
+			return value;
+
+		try {
+			if (regprocActuatorResponseArray == null) {
+				Response response = null;
+				JSONObject responseJson = null;
+				response = RestClient.getRequest(url, MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON);
+
+				responseJson = new JSONObject(response.getBody().asString());
+				regprocActuatorResponseArray = responseJson.getJSONArray("propertySources");
+			}
+			for (int i = 0, size = regprocActuatorResponseArray.length(); i < size; i++) {
+				JSONObject eachJson = regprocActuatorResponseArray.getJSONObject(i);
+				if (eachJson.get("name").toString().contains(section)) {
+					value = eachJson.getJSONObject(GlobalConstants.PROPERTIES).getJSONObject(key)
+							.get(GlobalConstants.VALUE).toString();
+					if (ConfigManager.IsDebugEnabled())
+						logger.info("Actuator: " + url + " key: " + key + " value: " + value);
+					break;
+				}
+			}
+			actuatorValueCache.put(actuatorCacheKey, value);
+
+			return value;
+		} catch (Exception e) {
+			logger.error(GlobalConstants.EXCEPTION_STRING_2 + e);
+			return "";
+		}
+
 	}
 
 }
