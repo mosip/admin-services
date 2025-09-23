182b27f665ae53a7b191d777edacf3409be13e68
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2025-09-23 18:18:05 +0530

    Added EnableCaching
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- admin/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/service/impl/SyncConfigDetailsServiceImpl.java
+++ admin/kernel-syncdata-service/src/main/java/io/mosip/kernel/syncdata/service/impl/SyncConfigDetailsServiceImpl.java
@@ -32,6 +32,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
@@ -62,6 +63,7 @@ import java.util.Map.Entry;
  */
 @RefreshScope
 @Service
+@EnableCaching
 public class SyncConfigDetailsServiceImpl implements SyncConfigDetailsService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SyncConfigDetailsServiceImpl.class);
