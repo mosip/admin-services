76172c0efa02567faf8d342fcdcc441fa27d5309
Nandhukumar <nandhukumare@gmail.com>
2025-12-05 23:51:34 +0530

    MOSIP-43917 | updated missed index scripts (#1326)
    Signed-off-by: Nandhukumar <nandhukumare@gmail.com>
    Co-authored-by: Praful Rakhade <prafulrakhade02@gmail.com>

--- db_upgrade_scripts/mosip_master/sql/1.2.1.4_to_1.3.0_rollback.sql
+++ db_upgrade_scripts/mosip_master/sql/1.2.1.4_to_1.3.0_rollback.sql
@@ -96,5 +96,8 @@ DROP INDEX IF EXISTS master.idx_user_detail_active_id;
 DROP INDEX IF EXISTS master.idx_user_detail_regcntr;
 DROP INDEX IF EXISTS master.idx_user_detail_regcntr_flags;
 DROP INDEX IF EXISTS master.idx_user_detail_regcntr_change;
+DROP INDEX IF EXISTS master.idx_ca_cert_domain;
+DROP INDEX IF EXISTS master.idx_ca_cert_isdeleted;
+DROP INDEX IF EXISTS master.idx_mac_master_sign_key_index_active;
 
 -- END ROLLBACK FOR PERFORMANCE OPTIMIZATION INDEXES
