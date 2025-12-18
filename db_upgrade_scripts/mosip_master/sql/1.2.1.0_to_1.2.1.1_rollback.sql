23c58981bc6e9903bd220ef3fd13edee6fb9d2d8
kameshsr <kameshsr1338@gmail.com>
2025-12-18 12:06:02 +0530

    MOSIP-44100 Corrected comment
    Signed-off-by: kameshsr <kameshsr1338@gmail.com>

--- db_upgrade_scripts/mosip_master/sql/1.2.1.0_to_1.2.1.1_rollback.sql
+++ db_upgrade_scripts/mosip_master/sql/1.2.1.0_to_1.2.1.1_rollback.sql
@@ -1,1 +1,3 @@
 \echo 'Rollback Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+-- We have intentionally removed DML statements from this rollback script as part of MOSIP-44100 (Previously tagged as v1.2.1.1).
+-- For more details Refer: https://mosip.atlassian.net/browse/MOSIP-44100
--- db_upgrade_scripts/mosip_master/sql/1.2.1.0_to_1.2.1.1_upgrade.sql
+++ db_upgrade_scripts/mosip_master/sql/1.2.1.0_to_1.2.1.1_upgrade.sql
@@ -1,1 +1,3 @@
-\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION' 
+-- We have intentionally removed DML statements from this upgrade script as part of MOSIP-44100 (Previously tagged as v1.2.1.1).
+-- For more details Refer: https://mosip.atlassian.net/browse/MOSIP-44100
