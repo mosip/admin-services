de12ec56764ce600df236f515f600b10a3525af5
Dhanendra Sahu <dhanendra.tech@gmail.com>
2026-05-15 17:32:04 +0530

    Fixed the audit issue
    Signed-off-by: Dhanendra Sahu <dhanendra.tech@gmail.com>

--- admin/admin-service/src/main/java/io/mosip/admin/packetstatusupdater/service/impl/AuditManagerProxyServiceImpl.java
+++ admin/admin-service/src/main/java/io/mosip/admin/packetstatusupdater/service/impl/AuditManagerProxyServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -117,7 +118,9 @@ public class AuditManagerProxyServiceImpl implements AuditManagerProxyService {
 		request.setRequesttime(LocalDateTime.now());
 		request.setRequest(auditManagerRequestDto);
 
-		HttpEntity<RequestWrapper<AuditManagerRequestDto>> entity = new HttpEntity<>(request);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<RequestWrapper<AuditManagerRequestDto>> entity = new HttpEntity<>(request, headers);
 
 		try {
 			restTemplate.postForEntity(auditmanagerapi, entity, String.class);
